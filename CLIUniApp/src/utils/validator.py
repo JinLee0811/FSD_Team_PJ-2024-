94569188ec902de323bd0a1a012598888628105f
Subin Oh <spreadmercy@gmail.com>
2024-04-23 21:36:53 +1000

    add colorize_text_code by subin

--- CLIUniApp/src/utils/validator.py
+++ CLIUniApp/src/utils/validator.py
@@ -1,1 +1,19 @@
 print("I'm starting now")
+
+# make color difference of commend
+# def colorize_text_blue(text):
+#     return f"\033[1;34m{text}\033[0m"
+
+# def colorize_text_green(text):
+#     return f"\033[1;32m{text}\033[0m"
+
+# def colorize_text_red(text):
+#     return f"\031[1;34m{text}\033[0m"
+
+# def colorize_text_yellow(text):
+#     return f"\033[1;33m{text}\033[0m"
+    
+    
+# print(colorize_text_blue("I'm done now"))
+# print(colorize_text_yellow("I'm done now"))
+
