1bbe7d46d5afca9e91cbfc287021325a4805d7c0
jeongjinLee <dlwjdwls0908.elice@gmail.com>
2024-05-01 17:49:16 +1000

    getting folder_GUI

--- CLIUniApp/src/admin_system.py
+++ CLIUniApp/src/admin_system.py
@@ -1,4 +1,11 @@
 # src/admin_system.py
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
 from colorama import Back, Fore, Style, init
 from controllers.admin_controller import (
     clear_database,
