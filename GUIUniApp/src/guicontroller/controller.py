5fb27c03c1ec1a10793f49d33288d9836b580d3a
jeongjinLee <dlwjdwls0908.elice@gmail.com>
2024-05-09 22:57:13 +1000

    error

--- GUIUniApp/src/guicontroller/controller.py
+++ GUIUniApp/src/guicontroller/controller.py
@@ -5,13 +5,16 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
+import random
 import tkinter as tk
 from tkinter import messagebox
+
+from guiframe.view import EnrolmentFrame, LoginFrame
+
 # from utils import logger
 from guimodel.model import GUIUniAppModel
-from guiframe.view import LoginFrame, EnrolmentFrame
-import random
 from subject import Subject
+
 
 class GUIUniAppController:
     def __init__(self, master):
@@ -25,30 +28,32 @@ class GUIUniAppController:
 
     def show_enrolment_frame(self):
         self.switch_frame(EnrolmentFrame)
-        
+
     def switch_frame(self, frame_class):
         if self.current_frame is not None:
             self.current_frame.destroy()
         self.current_frame = frame_class(self.master, self)
         self.current_frame.pack(fill=tk.BOTH, expand=True)
-        self.current_frame.bind('<Button-1>', lambda event: self.current_frame.focus_set())
+        self.current_frame.bind(
+            "<Button-1>", lambda event: self.current_frame.focus_set()
+        )
         self.current_frame.focus_set()
-            
+
     def authenticate(self, email, password):
         if self.model.authenticate(email, password):
             messagebox.showinfo("Login Successful", "You have successfully logged in!")
-            self.logged_in_user = self.model.logged_in_user 
+            self.logged_in_user = self.model.logged_in_user
             return True
         else:
             messagebox.showerror("Login Failed", "Incorrect Login credentials")
             return False
 
     def add_subject(self, subject):
-            return self.model.add_subject(subject)
+        return self.model.add_subject(subject)
 
     def remove_subject(self, subject_id):
         return self.model.remove_subject(subject_id)
-    
+
     def logout(self):
         self.model.logout()
         self.show_login_frame()
---
+++ GUIUniApp/src/main.py
@@ -0,0 +1,36 @@
+import datetime
+import os
+import sys
+import tkinter as tk
+
+from guicontroller.controller import GUIUniAppController
+from guiframe.view import LoadingScreen
+
+# 현재 스크립트가 위치한 디렉토리 경로를 가져옵니다.
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# 현재 스크립트의 부모 디렉토리(즉, src 디렉토리의 부모)의 경로를 얻습니다.
+parent_dir = os.path.dirname(current_dir)
+# 부모 디렉토리를 sys.path에 추가합니다.
+sys.path.append(parent_dir)
+
+
+class MainApplication(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("GUIUniApp")
+        self.geometry("375x612")
+        self.resizable(False, False)
+
+        # 로딩 화면 표시
+        self.loading_screen = LoadingScreen(self)
+        self.loading_screen.pack(fill=tk.BOTH, expand=True)
+        self.after(3000, self.show_login_frame)  # 3초 후 로그인 프레임 표시
+
+    def show_login_frame(self):
+        self.loading_screen.destroy()  # 로딩 화면 제거
+        self.controller = GUIUniAppController(self)
+
+
+if __name__ == "__main__":
+    app = MainApplication()
+    app.mainloop()
