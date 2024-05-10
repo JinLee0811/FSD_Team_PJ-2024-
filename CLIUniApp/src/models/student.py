6b3a5c4dd2f9a0fc3e35c2891e9c58b8e289d182
jeongjinLee <dlwjdwls0908.elice@gmail.com>
2024-05-10 20:41:18 +1000

    print data

--- CLIUniApp/src/models/student.py
+++ CLIUniApp/src/models/student.py
@@ -2,12 +2,10 @@ from models.subject import Subject
 
 
 class Student:
-    def __init__(self, name, email, password, student_id, grade=None, mark=None):
+    def __init__(self, name, email, password, student_id):
         self.name = name
         self.email = email
         self.password = password
         self.student_id = student_id
-        self.grade = grade
-        self.mark = mark
         self.subjects = []
         self.average_mark = None
